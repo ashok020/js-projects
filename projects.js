901679e92b894417f736f1b2c3f56b090a3862ab
ashok020 <61375148+ashok020@users.noreply.github.com>
2021-07-10 14:22:03 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -258,7 +258,7 @@ document.addEventListener("keyup", function (e) {
 });
 
 function clickFn(k) {
-  navigator.vibrate(100);
+  navigator.vibrate(10);
   result.style.backgroundColor = "aliceblue";
   if (ioString == "Error" || ioString == "NaN" || ioString == "Infinity") {
     ioString = "";
