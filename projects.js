f1d1686c640e7250d7e0aa4d56450f65d5a77f14
ashok020 <61375148+ashok020@users.noreply.github.com>
2021-07-20 11:54:02 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -179,10 +179,11 @@ function getTime() {
     ampm = "AM";
     if (hour < 10) hour = "0" + hour;
   }
-  if (hour == 12 && minutes == 0 && seconds == 0) {
-    ampm = "PM";
-  }
+
+if(hour ==12) {ampm = "PM";}
+
   if (hour > 12) {
+      ampm = "PM";
     hour = hour - 12;
   }
   if (minutes < 10) minutes = "0" + minutes;
